b43907d20ab54bb0eced894876ff61a285280c45
Admin <Admin@TEB12707>
2023-01-27 10:58:25 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uppgift54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] svar = new string[] { "Det är Berlin", "Det är inte en elefant", "Det husdjuret är en giraff", "Grönt", "rött", "sju stycken", "stockholm", "nej", "ja", "Abdi" };
+            Random random = new Random();
+            int index = random.Next(svar.Length);
+            Console.WriteLine("Skriv en fråga");
+            Console.ReadLine();
+            Console.WriteLine($"svar: {svar[index]}");
+        }
+    }
+}
